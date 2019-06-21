c1f2ad2497476876608fed3e7f2de49bdf3bb3f9
Dat Bui <dbdatbui@gmail.com>
2019-06-20 23:37:14 -0400

    image processing

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"image"
+	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/fogleman/gg"
 )
@@ -14,8 +17,8 @@ func main() {
 }
 
 func writeToImage(file_name string) {
+	im, err := gg.LoadImage(file_name)
 	const S = 1024
-	im, err := gg.LoadImage("src.jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,11 +30,46 @@ func writeToImage(file_name string) {
 	if err := dc.LoadFontFace("impact.ttf", 96); err != nil {
 		panic(err)
 	}
-	dc.DrawStringAnchored("Hello, world!", S/2, S/2, 0.5, 0.5)
 
 	dc.DrawRoundedRectangle(0, 0, 512, 512, 0)
 	dc.DrawImage(im, 0, 0)
-	dc.DrawStringAnchored("Hello, world!", S/2, S/2, 0.5, 0.5)
+	dc.DrawStringAnchored("Hello, world!", S/2, S-96, 0.5, 0.5)
 	dc.Clip()
 	dc.SavePNG("out.png")
+
+	pixel, err := getPixels(strings.NewReader(file_name))
+	for _, element := range pixel {
+		print("%s\n", element)
+	}
+}
+
+func getPixels(file io.Reader) ([][]Pixel, error) {
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	var pixels [][]Pixel
+	for y := 0; y < height; y++ {
+		var row []Pixel
+		for x := 0; x < width; x++ {
+			row = append(row, rgbaToPixel(img.At(x, y).RGBA()))
+		}
+		pixels = append(pixels, row)
+	}
+	return pixels, nil
+}
+
+func rgbaToPixel(r uint32, g uint32, b uint32, a uint32) Pixel {
+	return Pixel{int(r / 257), int(g / 257), int(b / 257), int(a / 257)}
+}
+
+type Pixel struct {
+	R int
+	G int
+	B int
+	A int
 }
